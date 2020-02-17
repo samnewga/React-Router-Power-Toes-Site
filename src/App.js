aaa8d5948acc6cfb6fa8c70738f9a7cc0d980223
samnewga <newgate90s@gmail.com>
2020-02-16 17:17:45 -0700

    1st

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Nav from './Nav'
+import Header from './Header'
+import Footer from './Footer'
+import HomePage from './HomePage'
+import './App.css'
+import AboutPage from './AboutPage'
+import WorkoutsPage from './WorkoutsPage'
+import NotFoundPage from './NotFoundPage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <nav>
+          <Nav />
+        </nav>
+        <header>
+          <Header />
+        </header>
+        <main>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/about' component={AboutPage} />
+            <Route path='/workouts' component={WorkoutsPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    )
+  }
 }
-
-export default App;
